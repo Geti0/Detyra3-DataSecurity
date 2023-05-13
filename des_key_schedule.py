6c969b7e65625d72ac243885360da880acc02582
getuarkelmendi <102876616+getuarkelmendi@users.noreply.github.com>
2023-05-13 18:10:21 +0200

    des_key_schedule update

---
+++ des_key_schedule.py
@@ -0,0 +1,16 @@
+# Fitimi i 16 subkey per secilin round te algoritmit DES
+
+# Ky eshte kodi gjeneral per implementimin e nje PBox
+# n paraqet numrin e input bitave
+# P eshte nje liste qe permban m integera
+# Integerat ne P jane ne mes te 1 dhe n
+# Input parametri x eshte nje integer n-bitesh
+# Outputi y eshte nje integer m-bitesh
+def pbox(x, P, n):
+    y = 0
+    for i in P:
+        y <<= 1
+        y ^= (x >> (n-i)) & 1
+    return y
+
+# PBoxat qe perdoren ne key schedule jane PC1, PC2, RHL1 dhe RHL2
